4407e04e0ebf9357472a52844361da712e342ec9
Mikevanlenthe <32332023+Mikevanlenthe@users.noreply.github.com>
2017-12-01 00:31:33 +0100

    print_multi_words fixed

--- translate_lexicon_v0.5/print_multiword_token.py
+++ translate_lexicon_v0.5/print_multiword_token.py
@@ -1,9 +1,3 @@
-import subprocess
-import os
-import sys
-from bs4 import UnicodeDammit
-import time
-
 # print good lines
 count = 0
 with open("NRC-emotion.txt", "r", encoding="utf-8") as infile:
@@ -14,15 +8,3 @@ with open("NRC-emotion.txt", "r", encoding="utf-8") as infile:
             count += 1
 
 print(count)
-
-# clean up
-with open("NRC-emotion.txt", "r", encoding="utf-8") as infile:
-    with open("NRC-emotion-cleaned.txt", "w", encoding="utf-8") as outfile:
-        for line in infile:
-            write_line = line
-            line = line.split()
-
-            if len(line) == 3:
-                print(line)
-                count += 1
-                outfile.write(write_line.rstrip() + "\n")  # write line to new file
