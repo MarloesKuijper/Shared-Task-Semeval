9e375cce7084548eb4b1e3ce6747ce8cc8bf54c7
MarloesKuijper <marloes.madelon@gmail.com>
2018-01-05 16:10:04 +0100

    add submissions folder, modify write_results, added some predictions for EI-reg and V-reg, also on Codalabs

--- python_scripts/write_results.py
+++ python_scripts/write_results.py
@@ -25,7 +25,7 @@ def get_files(files, ext, options):
 	final_list = []
 	for em in options:
 		for f in file_list:
-			if em in f: #found emotion in file, add
+			if em in f.lower(): #found emotion in file, add
 				final_list.append(f)
 				break #only add once per emotion
 	return final_list
@@ -51,7 +51,7 @@ def predict_and_write_output(features_train, features_test, original_test_file, 
 	## SVM test ##
 	clf = svm.SVR()
 	y_guess = clf.fit(Xtrain, Ytrain).predict(Xtest)
-	name = out_dir + "/" + task_name + "_es_" + " ".join([item for item in options if item in features_train]) + "_pred.txt"
+	name = out_dir + "/" + task_name + "_es_" + " ".join([item for item in options if item in features_train.lower()]) + "_pred.txt"
 	
 	with open(original_test_file, 'r', encoding="utf-8") as infile:
 		infile = infile.readlines()
@@ -75,9 +75,10 @@ if __name__ == "__main__":
 	training_feats = get_files(train_dir, ".csv", options)
 	test_feats = get_files(test_dir, ".csv", options)
 	original_txts = get_files(original_test, ".txt", options)
+	print(training_feats, test_feats, original_txts)
 	
 	assert len(training_feats) == len(test_feats) == len(original_txts) #lengths must be the same
 
 	for ix, file in enumerate(training_feats):
-		predict_and_write_output(training_feats[ix], test_feats[ix], original_txts[ix], out_dir, options, "EI-reg")
+		predict_and_write_output(training_feats[ix], test_feats[ix], original_txts[ix], out_dir, options, "V-reg")
 
